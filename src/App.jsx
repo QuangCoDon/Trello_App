34911534e38d25b1d0a5c673a402b00c5ef79aa5
QuangCoDon <quangluuyenho5b@gmail.com>
2025-10-15 17:26:42 +0700

    Changemode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useColorScheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Hello world!</div>
       <Typography variant="body2" color="text.secondary">Test Typography</Typography>
       <Button variant="text">Text</Button>
