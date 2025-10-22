7ad114ec1fcf86ce9d4be3ac6eadf0782c61ceb9
QuangCoDon <quangluuyenho5b@gmail.com>
2025-10-22 11:51:38 +0700

    CodeAppBarPart0

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,18 +1,27 @@
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
 import Box from '@mui/material/Box'
+import AppsIcon from '@mui/icons-material/Apps'
+import SvgIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
 
 function AppBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.light',
         width: '100%',
         height: (theme) => theme.trello.appBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between'
       }}
     >
-      <ModeSelect />
+      <Box>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+      </Box>
+      <Box>
+        <ModeSelect />
+      </Box>
     </Box>
   )
 }
